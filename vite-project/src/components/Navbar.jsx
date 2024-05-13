ffe115f0dfbf74e61bfb15549d750598892ca96c
Daniele <daniele.capa@gmail.com>
2024-05-13 13:58:57 +0200

    few adjustments

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.jpg';
 const Navbar = () => {
   return (
     <nav className="bg-[#434343] py-20 px-4 flex items-center justify-between">
-      {/* Logo (replace 'logo.jpg' with your actual logo image file) */}
+      {/* Logo */}
       <div className="ml-20">
         <img src={logo} alt="Logo" className="h-30 w-auto" />
       </div>
